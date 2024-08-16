6e50fbdf7d02518aab546e8d3451838465183832
gergoszaszvaradi <szaszvaradi.gergo@yahoo.com>
2024-08-16 10:45:32 +0300

    doom emacs keybinds cleanup

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -26,7 +26,7 @@
 ;;
 (setq doom-font (font-spec :family "CaskaydiaCove NF" :size 14 :weight 'regular)
       doom-variable-pitch-font (font-spec :family "CaskaydiaCove NF" :size 14))
-;;
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
@@ -78,25 +78,6 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-;; F14 as leader key
-(map! :map key-translation-map
-      "<XF86Launch5>" "C-c"
-      "<Launch5>" "C-c"
-      "<f14>" "C-c")
-
-;; Ivy
-(map! :after ivy
-      :map ivy-minibuffer-map
-      "C-o" #'ivy-occur)
-
-;; Copilot
-(after! copilot
-  (setq copilot-indent-offset-warning-disable t))
-(add-hook 'prog-mode-hook 'copilot-mode)
-(map! :after copilot :map copilot-completion-map
-      "<tab>" #'copilot-accept-completion
-      "TAB" #'copilot-accept-completion)
-
 ;; LSP
 (after! lsp-mode
   (setq lsp-headerline-breadcrumb-enable nil)
@@ -107,33 +88,10 @@
 (add-hook 'post-command-hook 
           (lambda ()
             (flycheck-popup-tip-mode -1)))
-(map! :after lsp-mode :map lsp-mode-map
-      "C-c g d" #'lsp-find-definition
-      "C-c g r" #'lsp-find-references
-      "C-c g i" #'lsp-find-implementation
-      "C-c h" #'lsp-ui-doc-glance ;; @todo: figure out lsp-ui-mode
-      "C-c ." #'lsp-execute-code-action)
 
 ;; Projectile
 ;; (after! projectile
 ;;   (projectile-buffers-filter-function 'projectile-buffers-with-file-or-process))
-(map! :after projectile
-      "C-<prior>" #'projectile-previous-project-buffer
-      "C-<next>" #'projectile-next-project-buffer
-      "C-\\" #'projectile-switch-to-buffer
-      :map projectile-mode-map
-      :prefix "C-p"
-      "p" #'projectile-switch-project
-      "a" #'projectile-add-known-project
-      "f" #'projectile-find-file
-      "b" #'projectile-compile-project
-      "r" #'projectile-run-project
-      "t" #'projectile-test-project)
-
-;; Counsel
-(map! :after counsel-projectile
-      "C-c f r" #'counsel-projectile-rg
-      "C-c f f" #'counsel-projectile-find-file)
 
 ;; Treemacs
 (after! treemacs
@@ -144,19 +102,6 @@
   (setq treemacs-filewatch-mode t)
   (setq treemacs-file-event-delay 500)
   (setq treemacs-display-current-project-exclusively t))
-(map! "C-e" #'treemacs)
-(map! :after treemacs
-      :map treemacs-mode-map
-      "<right>" #'treemacs-RET-action ;; Open folder
-      "<left>" #'treemacs-COLLAPSE-action ;; Focus parent or close folder
-      "r" #'treemacs-rename-file ;; Rename entry
-      "p" #'treemacs-peek-mode) ;; Peek file
-
-;; Undo tree
-(map! :after undo-tree
-      "C-z" #'undo-tree-undo
-      "C-S-z" #'undo-tree-redo
-      "C-M-z" #'undo-tree-visualize)
 
 ;; HL TODO
 (after! hl-todo
@@ -171,66 +116,10 @@
           ("hack"   . "#A652FF")
           ("fixme"  . "#FF8343"))))
 
-;; Multiple cursors
-(map! :after multiple-cursors
-      "M-<mouse-1>" #'mc/add-cursor-on-click
-      "C-d" #'mc/mark-next-like-this
-      "C-S-d" #'mc/mark-all-like-this)
+;; Copilot
+(after! copilot
+  (setq copilot-indent-offset-warning-disable t))
+(add-hook 'prog-mode-hook 'copilot-mode)
 
-;; Keybindings
-
-;; F1 for commands
-(map! :map key-translation-map [f1] "M-x")
-
-;; ESC for abort
-(map! :map key-translation-map "ESC" "C-g")
-
-;; Find and Replace
-(map! "C-f" #'isearch-forward)
-(map! "C-S-f" #'isearch-backward)
-(map! :map isearch-mode-map "C-f" #'isearch-repeat-forward)
-(map! :map key-translation-map "C-h" "C-M-%")
-
-;; Save with C-s
-(map! "C-s" #'save-buffer)
-
-;; Editing
-(map! "C-S-k" #'kill-whole-line)
-(map! :map key-translation-map "C-c" "C-c x")
-(map! "C-c x" (lambda () (interactive) (let (ξp1 ξp2 (deactivate-mark nil))
-                                         (if current-prefix-arg
-                                             (setq ξp1 (point-min) ξp2 (point-max))
-                                           (if (use-region-p)
-                                               (progn (setq ξp1 (region-beginning) ξp2 (region-end))
-                                                      (kill-ring-save ξp1 ξp2))
-                                             (progn
-                                               (setq ξp1 (line-beginning-position) ξp2 (line-end-position))
-                                               (kill-ring-save ξp1 ξp2)
-                                               (kill-append "\n" t)))))))
-(map! "C-v" #'yank)
-(map! "C-/" #'comment-line)
-(map! "C-a" #'mark-whole-buffer)
-
-;; Navigation
-(map! [home] #'back-to-indentation)
-(map! "C-<left>" (lambda () (interactive "^") (forward-same-syntax -1))) ;; Jump left until different symbol type
-(map! "C-<right>" (lambda () (interactive "^") (forward-same-syntax 1))) ;; Jump right until different symbol type
-(map! "M-<right>" #'windmove-right) ;; Focus right window
-(map! "M-<left>" #'windmove-left) ;; Focus left window
-(global-unset-key (kbd "C-w"))
-(map! "C-w w" #'delete-window) ;; Close window
-(map! "C-w C-w" #'delete-window)
-(map! "C-w q" #'kill-buffer-and-window) ;; Close window and buffer
-(map! "C-w C-q" #'kill-buffer-and-window)
-(map! "C-w v" #'split-window-below) ;; Split vertically
-(map! "C-w C-v" #'split-window-below)
-(map! "C-w h" #'split-window-right) ;; Split horizontally
-(map! "C-w C-h" #'split-window-right)
-(map! "C-w <left>" #'windmove-left) ;; Focus left window
-(map! "C-w <right>" #'windmove-right) ;; Focus right window
-(map! "C-w <up>" #'windmove-up) ;; Focus top window
-(map! "C-w <down>" #'windmove-down) ;; Focus bottom window
-(map! "C-w f" #'toggle-maximize-buffer) ;; Fullscreen window
-(map! "C-q" #'kill-this-buffer) ;; Close buffer
-(map! "C-l" #'goto-line) ;; Go to line
-(map! "C-n" #'scratch-buffer) ;; New buffer
+;; Load keybinds
+(load-file "~/.doom.d/keybinds.el")
---
+++ doom/.doom.d/keybinds.el
@@ -0,0 +1,138 @@
+;; General defaults
+
+;; F14 as leader key
+(map! :map key-translation-map
+      "<XF86Launch5>" "C-c"
+      "<Launch5>" "C-c"
+      "<f14>" "C-c")
+
+;; F1 for commands
+(map! :map key-translation-map [f1] "M-x")
+
+;; ESC to abort
+(map! :map key-translation-map "ESC" "C-g")
+
+;; Save with C-s
+(map! "C-s" #'save-buffer)
+
+;; New buffer
+(map! "C-n" #'scratch-buffer)
+
+;; Quit buffer
+(map! "C-q" #'kill-this-buffer)
+
+;; Select all
+(map! "C-a" #'mark-whole-buffer)
+
+;; Copy/Paste
+(map! :map key-translation-map "C-c" "C-c x")
+(map! "C-c x" (lambda () (interactive) (let (p1 p2 (deactivate-mark nil))
+                                         (if current-prefix-arg
+                                             (setq p1 (point-min) p2 (point-max))
+                                           (if (use-region-p)
+                                               (progn (setq p1 (region-beginning) p2 (region-end))
+                                                      (kill-ring-save p1 p2))
+                                             (progn
+                                               (setq p1 (line-beginning-position) p2 (line-end-position))
+                                               (kill-ring-save p1 p2)
+                                               (kill-append "\n" t)))))))
+(map! "C-v" #'yank)
+
+;; Find and Replace
+(map! "C-f" #'isearch-forward)
+(map! "C-S-f" #'isearch-backward)
+(map! :map isearch-mode-map "C-f" #'isearch-repeat-forward)
+(map! :map key-translation-map "C-h" "C-M-%")
+
+;; Undo/Redo
+(map! :after undo-tree
+      "C-z" #'undo-tree-undo
+      "C-S-z" #'undo-tree-redo
+      "C-M-z" #'undo-tree-visualize)
+
+;; Minibuffer to buffer
+(map! :after ivy
+      :map ivy-minibuffer-map
+      "C-o" #'ivy-occur)
+
+;; Navigation
+
+;; Home to indentation
+(map! "<home>" #'back-to-indentation)
+
+;; Jump words
+(map! "C-<left>" (lambda () (interactive "^") (forward-same-syntax -1)))
+(map! "C-<right>" (lambda () (interactive "^") (forward-same-syntax 1)))
+
+;; Window navigation
+(map! :leader :prefix "w"
+      "w" #'delete-window
+      "q" #'kill-buffer-and-window
+      "h" #'split-window-right
+      "v" #'split-window-below
+      "f" (lambda () (interactive)
+            (if (= 1 (length (cl-remove-if #'treemacs-is-treemacs-window? (window-list))))
+                (jump-to-register '_)
+              (progn
+                (window-configuration-to-register '_)
+                (delete-other-windows))))
+      "m <left>" #'windmove-left
+      "m <right>" #'windmove-right
+      "m <up>" #'windmove-up
+      "m <down>" #'windmove-down)
+
+;; Go to line
+(map! :leader "g l" #'goto-line)
+
+;; LSP navigation
+(map! :after lsp-mode :map lsp-mode-map :leader
+      "g d" #'lsp-find-definition
+      "g r" #'lsp-find-references
+      "g i" #'lsp-find-implementation
+      ;; "h" #'lsp-ui-doc-glance ;; @todo: figure out lsp-ui-mode
+      "." #'lsp-execute-code-action)
+
+;; Projectile
+(map! :after projectile
+      "C-<prior>" #'projectile-previous-project-buffer
+      "C-<next>" #'projectile-next-project-buffer
+      "C-\\" #'projectile-switch-to-buffer
+      :map projectile-mode-map
+      :leader
+      :prefix "p"
+      "p" #'projectile-switch-project
+      "a" #'projectile-add-known-project
+      "f" #'projectile-find-file
+      "b" #'projectile-compile-project
+      "r" #'projectile-run-project
+      "t" #'projectile-test-project)
+(map! :after counsel-projectile
+      "C-c f r" #'counsel-projectile-rg
+      "C-c f f" #'counsel-projectile-find-file)
+
+;; Treemacs
+(map! "C-e" #'treemacs)
+(map! :map treemacs-mode-map
+      "<right>" #'treemacs-RET-action
+      "<left>" #'treemacs-COLLAPSE-action
+      "r" #'treemacs-rename-file
+      "p" #'treemacs-peek-mode)
+
+;; Multiple cursors
+(map! :after multiple-cursors
+      "M-<mouse-1>" #'mc/add-cursor-on-click
+      "C-d" #'mc/mark-next-like-this
+      "C-S-d" #'mc/mark-all-like-this)
+
+;; Editing
+
+;; Comment/uncomment
+(map! "C-/" #'comment-line)
+
+
+;; Other
+
+;; Copilot
+(map! :after copilot :map copilot-completion-map
+      "<tab>" #'copilot-accept-completion
+      "TAB" #'copilot-accept-completion)
